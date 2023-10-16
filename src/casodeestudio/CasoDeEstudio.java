16f48ab508cef8b0adc8f2388b3d6cc99dd935d9
USUARIO <USUARIO@DESKTOP-FI7OAKP>
2023-10-15 15:42:59 -1200

    Subir caso de estudio

--- src/casodeestudio/CasoDeEstudio.java
+++ src/casodeestudio/CasoDeEstudio.java
@@ -15,6 +15,8 @@ public class CasoDeEstudio {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hello world");
+                
     }
     
 }
